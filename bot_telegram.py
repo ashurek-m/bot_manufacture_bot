6fd05691ed13d8a516d5fb87a0ce0a636bdef7f3
oshurek_m <max.ashurek@gmil.com>
2022-03-10 17:05:18 +0300

    начало создания клиенской части

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,13 +6,28 @@ import os
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
+'''***************КЛИЕНСКАЯ ЧАСТЬ*******************'''
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def commands_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'boom')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом только через ЛС, напиши ему:\nhttps://t.me/smithMaxBot')
+
+
+'''***************АДМИНСКАЯ ЧАСТЬ*******************'''
+
+'''***************ОБЩАЯ ЧАСТЬ*******************'''
 
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    #await message.answer(message.text)
+    # await message.answer(message.text)
     await message.reply(message.text)
-    #await bot.send_message(message.from_user.id,message.text)
+    # await bot.send_message(message.from_user.id,message.text)
 
 
 executor.start_polling(dp, skip_updates=True)
