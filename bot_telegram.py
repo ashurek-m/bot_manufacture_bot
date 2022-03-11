ac6163845e55117c9eab100309a1ccbb077bfcc5
oshurek_m <max.ashurek@gmil.com>
2022-03-11 16:10:58 +0300

    разбивка бота на модули

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,33 +1,9 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
-import os
-
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
-'''***************КЛИЕНСКАЯ ЧАСТЬ*******************'''
+from create_bot import dp
+from handlers import client, other
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def commands_start(message: types.Message):
-    try:
-        await bot.send_message(message.from_user.id, 'boom')
-        await message.delete()
-    except:
-        await message.reply('Общение с ботом только через ЛС, напиши ему:\nhttps://t.me/smithMaxBot')
-
-
-'''***************АДМИНСКАЯ ЧАСТЬ*******************'''
-
-'''***************ОБЩАЯ ЧАСТЬ*******************'''
-
-
-@dp.message_handler()
-async def echo_send(message: types.Message):
-    # await message.answer(message.text)
-    await message.reply(message.text)
-    # await bot.send_message(message.from_user.id,message.text)
-
+client.register_handler_client(dp)
+other.register_handler_other(dp)
 
 executor.start_polling(dp, skip_updates=True)
---
+++ handlers/other.py
@@ -0,0 +1,13 @@
+from aiogram import types, Dispatcher
+from create_bot import dp
+
+
+# @dp.message_handler()
+async def echo_send(message: types.Message):
+    # await message.answer(message.text)
+    await message.reply(message.text)
+    # await bot.send_message(message.from_user.id,message.text)
+
+
+def register_handler_other(dp_1: Dispatcher):
+    dp_1.register_message_handler(echo_send)
