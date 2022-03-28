1cf92f0400c9a5df849fd03bef7ff4a50e4ef833
oshurek_m <max.ashurek@gmil.com>
2022-03-28 15:42:35 +0300

    создание машины состояния для загрузки нового продукта

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,6 @@
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram import types
+from aiogram import types, Dispatcher
 from create_bot import dp
 
 
@@ -11,7 +11,53 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
-@dp.message_handler(commands='загрузить', state=None)
+# Начало диалога загрузки нового пункта меню
+# @dp.message_handler(commands='загрузить', state=None)
 async def cm_start(message: types.Message):
     await FSMAdmin.photo.set()
     await message.reply('Загрузи фото')
+
+
+# Ловим первый ответ от пользователя
+# @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+async def load_photo(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['photo'] = message.photo[0].file_id
+    await FSMAdmin.next()
+    await message.reply('Теперь добавь название')
+
+
+# Ловим второй ответ от пользователя
+# @dp.message_handler(state=FSMAdmin.name)
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Введите описание')
+
+
+# Ловим третий ответ от пользователя
+# @dp.message_handler(state=FSMAdmin.description)
+async def load_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Теперь введите цену')
+
+
+# Ловим четвертый ответ от пользователя
+# @dp.message_handler(state=FSMAdmin.price)
+async def load_price(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = float(message.text)
+    async with state.proxy() as data:
+        await message.reply(str(data))
+    await state.finish()
+
+
+def register_handler_admin(dp_1: Dispatcher):
+    dp_1.register_message_handler(cm_start, commands=['загрузить'], state=None)
+    dp_1.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
+    dp_1.register_message_handler(load_name, state=FSMAdmin.name)
+    dp_1.register_message_handler(load_description, state=FSMAdmin.description)
+    dp_1.register_message_handler(load_price, state=FSMAdmin.price)
